8bcbd9ec88f2ebfd168a7ff4a1f60a4d28b6e048
mdivana <nikamdivani@icloud.com>
2023-06-18 19:38:14 +0400

    morcha gatavda

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Nav from './components/Nav/Nav'
+// import "bootstrap/dist/css/bootstrap.css";
 import './styles.css'
 
 export default function App() {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,6 +19,5 @@ export default function Header() {
                 </div>
             </div>
         </header>
-
     )
 }
